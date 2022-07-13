8585fc2dfa2d793c8e64769ead3db439bb9528a2
Jake Edgar <jakeedgar1012@gmail.com>
2022-07-13 14:08:23 -0700

    WIP revise file structure

--- src/App.js
+++ src/App.js
@@ -1,73 +1,28 @@
-import { useEffect, useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
-import { db } from './firebase'
-import { 
-  addDoc,
-  collection, 
-  getDocs,
-  updateDoc,
-  doc,
-  deleteDoc,
-  } from 'firebase/firestore'
-
-function App() {
-  const [newUserName, setNewUserName] = useState('');
-  const [newUserAge, setNewUserAge] = useState(0);
-  const [users, setUsers] = useState([]);
-  const usersCollectionRef = collection(db, 'users');
+import Home from "./pages/home";
 
 
-  const createUser = async () => {
-    await addDoc(usersCollectionRef, { name: newUserName, age: Number(newUserAge) });
-  };
-
-  const updateUser = async (id, age) => {
-    const userDoc = doc(db, "users", id)
-    const newFields = {age: age + 1}
-    await updateDoc(userDoc, newFields)
-  };
-
-
-  const deleteUser = async (id) => {
-    const userDoc = doc(db, "users", id) 
-    await deleteDoc(userDoc);
-  };
-
-  useEffect(() => {
-    const getUsers = async () => {
-      const data = await getDocs(usersCollectionRef);
-      setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id })));
-    };
-
-    getUsers()
-  })
+export default function App() {
   return (
-    <div className="App">
-      <input 
-      placeholder="Enter Name..." 
-      onChange={(e) => {
-        setNewUserName(e.target.value)}} 
-      />
-      <input 
-      type="number" 
-      placeholder="Enter Age..." 
-      onChange={(e) => {
-        setNewUserAge(e.target.value)
-      }}
-      />
-      <button onClick={createUser}>Create User</button>
-      {users.map((user) => {
-      return (
-        <div>
-          {" "}
-          <h1>Name: {user.name}</h1>
-          <h1>Age: {user.age}</h1>
-          <button onClick={() => {updateUser(user.id, user.age)}}>Increase Age</button>
-          <button onClick={() => {deleteUser(user.id)}}>Delete Me</button>
-        </div>
-      );
-      })}
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+          </ul>
+        </nav>
+          <Route path="/">
+            <Home />
+          </Route>
+      </div>
+    </Router>
   );
 }
-export default App;
