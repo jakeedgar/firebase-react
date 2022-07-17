b3754f86f5c85abd24f6e70bd44c18ebcd3e599d
Jake Edgar <jakeedgar1012@gmail.com>
2022-07-17 07:50:30 -0700

    add user auth and a login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,22 @@
+import React from 'react';
+import { auth, provider } from '../../firebase';
+import { signInWithPopup } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
-
-const Login = () => {
+function Login({ setIsAuth }) {
+  const navigate = useNavigate();
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, provider).then((result) => {
+      localStorage.setItem("isAuth", true);
+      setIsAuth(true);
+      navigate('/');
+    })
+  }
 
   return(
     <div className="loginPage">
-      Sign in to Continue:
-      <button className="login-with-google">
+      <p>Sign in to Continue:</p>
+      <button className="login-with-google-btn" onClick={signInWithGoogle}>
         Sign in:
       </button>
     </div>
